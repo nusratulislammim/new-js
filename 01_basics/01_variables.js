c5cd1e7f3974bf456ce65085f94ddf2d481da530
Nusratul Islam Mim <nusratulislammim@gmail.com>
2025-09-13 17:09:03 +0000

    variables & Data types

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,31 @@
+const accountId = 111111
+let accountEmail = "nusratul@google.com"
+var accountPassword = "12345"
+accountCity = "Trishal"
+
+let accountState;
+
+
+// js dont work in scope basis... scope - {}
+//let>>var
+/*
+prefer not to use var 
+bcs of issues in block scope and functional scope
+*/
+
+
+
+
+//accountId = 3
+accountEmail = "meem@gmail.com"
+accountPassword = "1234"
+accountCity = "Dhaka"
+
+
+//console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
+
+
+
