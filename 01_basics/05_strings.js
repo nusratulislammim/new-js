55be8729943a5a2b7436669a27020c74bb478feb
Nusratul Islam Mim <nusratulislammim@gmail.com>
2025-09-15 01:16:17 +0600

    Create 05_strings.js
    string basic

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,22 @@
+const name = "Meem"
+const repoCount = 50
+
+console.log(name+repoCount+"value");
+
+console.log(`Hello this is ${name} and my repo count is ${repoCount}`);
+console.log(`my repo: ${repoCount}`)
+console.log(`my name: ${name}`);
+
+const gameName = new String('meem.com');
+
+console.log(gameName[2]);
+console.log(gameName._proto_);
+
+console.log(gameName.length);
+console.log(gameName.indexOf('t'));
+console.log(gameName.indexOf('e'));
+console.log(gameName.charAt(2));
+
+
+
+
