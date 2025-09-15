496edc192a6481ad74ae7283416f91bd43344849
Nusratul Islam Mim <nusratulislammim@gmail.com>
2025-09-15 09:50:15 +0600

    Update 05_strings.js
    Strings

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -15,7 +15,42 @@ console.log(gameName._proto_);
 console.log(gameName.length);
 console.log(gameName.indexOf('t'));
 console.log(gameName.indexOf('e'));
-console.log(gameName.charAt(2));
+console.log(gameName.indexOf('M'));
+
+console.log(gameName.charAt(2));//2 s
+console.log(gameName.charAt(5));
+
+const newS = gameName.substring(1,4);
+console.log((newS));
+console.log(gameName.substring(-8,3));
+
+const anotherSt = gameName.slice(-8,4);
+console.log(anotherSt);
+
+const newS2 = "    Meem   ";
+console.log(newS);//print nothing for space
+console.log(newS2.trim());//remove spaces
+
+const url = "https://www.youtube.com/watch?v=X7hDBhd_L5U&list=PLu71SKxNbfoBuX3f4EOACle2y-tRC5Q37&index=6";
+
+console.log(url.replace('&list','-'));
+
+console.log(url.includes('haha'));//false
+console.log(url.includes('index'));//true
+
+console.log(gameName.split('-'));
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
