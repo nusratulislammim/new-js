72781c62219e63625068c1531afd1e2a36a51cfb
Nusratul Islam Mim <nusratulislammim@gmail.com>
2025-09-17 21:18:23 +0600

    Create jsonApiIntro_v18.js

---
+++ 02_basics/jsonApiIntro_v18.js
@@ -0,0 +1,18 @@
+ const course = {
+    coursename: "js",
+    price: "100",
+    courseInstructor: "Who-cares"
+ }
+
+ //console.log(course.courseInstructor);
+ 
+ const {courseInstructor: instructor} = course;
+ //console.log(courseInstructor);
+ console.log(instructor);
+
+ 
+ 
+
+ 
+ 
+ 
